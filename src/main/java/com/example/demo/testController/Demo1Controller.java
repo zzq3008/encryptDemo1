eabde0a01c364a8ff4339feea3be53b759c3ab36
zzq <zzq@email.com>
2019-03-04 13:33:39 +0800

    444

--- src/main/java/com/example/demo/testController/Demo1Controller.java
+++ src/main/java/com/example/demo/testController/Demo1Controller.java
@@ -28,6 +28,7 @@ public class Demo1Controller {
 		Map<String, String> map = new HashMap<>();
 		map.put("key1", "hello");
 		map.put("name", "lily1");
+		map.put("name2", "lily2");
 		return map;
 	}
 	
